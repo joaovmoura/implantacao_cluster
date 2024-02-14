7c1fbbc7e90c8cdc1c81424a0b74eb3c27797607
joaovmoura <joao.vitor.figueiredo@ccc.ufcg.edu.br>
2024-02-14 11:30:17 -0300

    Containerd setup

---
+++ Containerd/setup.sh
@@ -0,0 +1,75 @@
+# Instalação do ambiente kubernetes usando Containerd como container runtime
+
+
+# ----- Instalação dos módulos do kernel -----
+
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+
+# ----- Configuração dos parâmetros do sysctl -----
+
+
+# Configuração dos parâmetros do sysctl, fica mantido mesmo com reebot da máquina.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Aplica as definições do sysctl sem reiniciar a máquina
+sudo sysctl --system
+
+
+# ----- Instalação -----
+
+
+# Adicionando o repositório do Docker
+# Instalação de pré requisitos
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg --yes
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Configurando o repositório
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt update && sudo apt install containerd.io -y
+
+
+# Configuração padrão do Containerd
+sudo mkdir -p /etc/containerd && containerd config default | sudo tee /etc/containerd/config.toml 
+
+
+# Alterar o arquivo de configuração pra configurar o systemd cgroup driver. 
+# Sem isso o Containerd não gerencia corretamente os recursos computacionais e vai reiniciar em loop
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+
+
+# ----- Instalação do kubeadm, kubelet and kubectl -----
+
+
+sudo apt-get update && \
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update && \
+sudo apt-get install -y kubelet kubeadm kubectl 
+
+sudo apt-mark hold kubelet kubeadm kubectl 
