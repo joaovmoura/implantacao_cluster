fa20f943424985d3e28c19166ce935543551b9ef
joaovmoura <joao.vitor.figueiredo@ccc.ufcg.edu.br>
2024-03-07 14:31:59 -0300

    consertando script docker

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -1,42 +1,28 @@
-# ----- Adicionando o repositório do Docker e instalando pré-requisitos -----
+#   Instalando docker
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+  sudo apt-get update
+  sudo apt-get install -y docker.io
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#   Suporte HTTPS caso necessario
 
-sudo apt-get update
+  sudo apt-get update 
+  sudo apt-get install -y apt-transport-https
 
-# ----- Instalação do Docker -----
+#   Instalando curl
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  sudo apt-get install curl
 
-# ----- Configuração do Docker -----
+# Instalação do kubernetes
 
-# Adicionando o usuário atual ao grupo docker para evitar usar sudo
-sudo usermod -aG docker $USER
+  sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+  
+  sudo apt-get install -y apt-transport-https ca-certificates curl
 
-# Reiniciando o Docker para aplicar as configurações
-sudo systemctl restart docker
+  curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+  
+  echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-# ----- Configuração dos parâmetros do sysctl -----
-
-# Configurando os parâmetros do sysctl para o Docker
-echo "net.bridge.bridge-nf-call-iptables = 1" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
-
-# Aplica as definições do sysctl sem reiniciar a máquina
-sudo sysctl --system
-
-# ----- Instalação do kubeadm, kubelet e kubectl -----
-
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-
-# Marcar as versões instaladas para evitar atualizações automáticas
-sudo apt-mark hold kubelet kubeadm kubectl
-
+  sudo apt-get update
+  sudo apt-get install -y kubectl kubeadm kubelet
+  sudo apt-mark hold kubelet kubeadm kubectl 
